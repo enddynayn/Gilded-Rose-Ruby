dff11a1c4d8b3ee8a98de69b6fe9df6563fd74eb
eNddy <enddynayn@gmail.com>
2020-03-18 18:54:11 -0700

    refact(*): tick method
    feat(conjured): add congured item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,6 +1,11 @@
 class GildedRose
   attr_reader :name, :days_remaining, :quality
 
+  AGED_BRIE = "Aged Brie"
+  BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+  SULFRAS = "Sulfuras, Hand of Ragnaros"
+  CONJURED = "Conjured Mana Cake"
+
   def initialize(name:, days_remaining:, quality:)
     @name = name
     @days_remaining = days_remaining
@@ -8,48 +13,79 @@ class GildedRose
   end
 
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
+    return update_generic if generic?
+
+    return if sulfras?
+  
+    return update_congured if congured?
+
+    return update_backstage_passes if backstage_passes?
+  
+    return update_aged_brie if aged_brie?
+  end
+
+  private
+  
+  attr_writer :days_remaining
+
+  def decrement_day
+    @days_remaining -= 1
+  end
+
+  def update_congured
+    return decrement_day if quality == 0
+    return decrement_day if quality == 0 and days_remaining == 0
+    @quality -= 2 if quality == 10 and days_remaining <= 0
+    @quality -= 2
+
+    decrement_day
+  end
+
+  def update_backstage_passes
+    if quality < 50
+      @quality += 1 if days_remaining.between?(11, 49)
+      @quality += 3 if days_remaining < 6
+      @quality += 2 if days_remaining.between?(6, 10)
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
+    
+    decrement_day
+  
+    @quality = 0 if days_remaining < 0
+  end
+
+  def update_generic
+    @quality -= 1 if @quality > 0
+  
+    decrement_day
+
+    @quality -= 1 if days_remaining < 0 and @quality > 0
+  end
+
+  def update_aged_brie
+    @quality += 1 if quality < 50
+  
+    decrement_day
+
+    @quality += 1 if days_remaining < 0 and quality < 50
+  end
+
+  def congured?
+    name == CONJURED
+  end
+
+  def backstage_passes? 
+    name == BACKSTAGE_PASSES
+  end
+
+  def aged_brie?
+    name == AGED_BRIE
+  end
+
+  def sulfras?
+    name == SULFRAS
+  end
+
+  def generic?
+    ![SULFRAS, AGED_BRIE, BACKSTAGE_PASSES, CONJURED].include?(@name)
   end
 end
